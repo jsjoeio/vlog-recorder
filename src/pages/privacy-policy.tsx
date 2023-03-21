1ea65f653407e468ed2191f5566fbb461d4f1b01
Joe Previte <jjprevite@gmail.com>
2023-03-21 04:52:03 +0000

    fix privacy policy

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -20,13 +20,13 @@ export default function PrivacyPolicy() {
       <p>
         Vlog Recorder follows a standard procedure of using log files. These
         files log visitors when they visit websites. All hosting companies do
-        this and a part of hosting services' analytics. The information
+        this and a part of hosting services&apos; analytics. The information
         collected by log files include internet protocol (IP) addresses, browser
         type, Internet Service Provider (ISP), date and time stamp,
         referring/exit pages, and possibly the number of clicks. These are not
         linked to any information that is personally identifiable. The purpose
         of the information is for analyzing trends, administering the site,
-        tracking users' movement on the website, and gathering demographic
+        tracking users&apos; movement on the website, and gathering demographic
         information. Our Privacy Policy was created with the help of the{" "}
         <a href="https://www.privacypolicygenerator.org">
           Privacy Policy Generator
@@ -45,7 +45,7 @@ export default function PrivacyPolicy() {
         Third-party ad servers or ad networks uses technologies like cookies,
         JavaScript, or Web Beacons that are used in their respective
         advertisements and links that appear on Vlog Recorder, which are sent
-        directly to users' browser. They automatically receive your IP address
+        directly to users&apos; browser. They automatically receive your IP address
         when this occurs. These technologies are used to measure the
         effectiveness of their advertising campaigns and/or to personalize the
         advertising content that you see on websites that you visit.
@@ -59,7 +59,7 @@ export default function PrivacyPolicy() {
       <h2>Third Party Privacy Policies</h2>
 
       <p>
-        Vlog Recorder's Privacy Policy does not apply to other advertisers or
+        Vlog Recorder&apos;s Privacy Policy does not apply to other advertisers or
         websites. Thus, we are advising you to consult the respective Privacy
         Policies of these third-party ad servers for more detailed information.
         It may include their practices and instructions about how to opt-out of
@@ -69,11 +69,11 @@ export default function PrivacyPolicy() {
       <p>
         You can choose to disable cookies through your individual browser
         options. To know more detailed information about cookie management with
-        specific web browsers, it can be found at the browsers' respective
+        specific web browsers, it can be found at the browsers&apos; respective
         websites.
       </p>
 
-      <h2>Children's Information</h2>
+      <h2>Children&apos;s Information</h2>
 
       <p>
         Another part of our priority is adding protection for children while
